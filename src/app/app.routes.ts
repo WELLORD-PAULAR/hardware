f151773d510292b6243e0c1df09fd4ac8c4e7abd
Wellord Paular <paular_w@ca01.ub.lan>
2025-09-30 12:46:48 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Cart } from './cart/cart';
+import { Checkout } from './checkout/checkout';
+import { Home } from './home/home';
+import { ProductList } from './product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Home},
+  {path: 'products', component: ProductList},
+  {path: 'cart', component: Cart},
+  {path: 'checkout', component: Checkout}
+];
